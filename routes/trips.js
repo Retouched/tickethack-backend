39fdfd2cc94f76ec2897b6bf7d8a14f836e98aaa
Romain <pro.vuillaume@gmail.com>
2023-06-28 13:26:28 +0200

    Ajout de commentaires dans la route pour rechercher les trips

--- routes/trips.js
+++ routes/trips.js
@@ -6,10 +6,9 @@ const Trip = require('../models/trips');
 
 
 // ROUTE => GET : ALL TRIPS
+// **** A RAJOUTER => LA CONDITION SUR LA DATE ****
 router.get('/:departure/:arrival', (req, res) => {
-
     Trip.find({departure: req.params.departure, arrival: req.params.arrival}).then(tripsFounded => {
-        
         res.json({allTripsFounded: tripsFounded})
     })
 })
