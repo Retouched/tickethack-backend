615b1b530628d1b0efab34f0837be46b5cce8309
Romain <pro.vuillaume@gmail.com>
2023-06-28 13:23:39 +0200

    Creation de la route pour rechercher les trips dans trips.js

--- routes/trips.js
+++ routes/trips.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-const fetch = require('node-fetch');
 const Trip = require('../models/trips');
 
 
 
+// ROUTE => GET : ALL TRIPS
+router.get('/:departure/:arrival', (req, res) => {
 
-
-
-
-
+    Trip.find({departure: req.params.departure, arrival: req.params.arrival}).then(tripsFounded => {
+        
+        res.json({allTripsFounded: tripsFounded})
+    })
+})
 
 
 
