c36de96126369f9b1386427a69538a931e9ff74d
Romain <pro.vuillaume@gmail.com>
2023-06-28 10:15:43 +0200

    Ceation du fichier trips.js dans le dossier routes avec requir: express, router, model trip, fetch + module.exports à la fin du fichier

---
+++ routes/trips.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const fetch = require('node-fetch');
+const Trip = require('../models/trips');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
